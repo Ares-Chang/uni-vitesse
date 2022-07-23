335ab7e73c66294721c341121d5f7f9d9707c927
chang <2622310479@qq.com>
2022-07-23 19:47:13 +0800

    chore: 配置 @ 路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+// 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // 使用路径别名时想要省略的后缀名，可以自己 增减
+  },
 })
