0d9e0d1abd7ce0f55cfda049bbb7536b4d1589d1
chang <2622310479@qq.com>
2022-07-24 20:09:55 +0800

    chore: unocss 预设配置（属性模式 & icons & @apply 功能）

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,8 +6,15 @@ const title = ref('Hello')
 <template>
   <view class="flex flex-col items-center justify-center">
     <image class="w-[200rpx] h-[200rpx] m-t-[200rpx]" src="/static/logo.png" />
-    <view class="m-t-xs">
-      <text class="title color-pink">{{ title }}</text>
+    <view class="m-t-xs flex gap-2">
+      <view class="i-mdi-mine color-pink"></view>
+      <view class="title" font-bold>{{ title }}</view>
     </view>
   </view>
 </template>
+
+<style lang="scss" scoped>
+.title {
+  @apply: color-yellow;
+}
+</style>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,22 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 import presetWxapp from 'unocss-preset-wxapp'
-export default {
-  presets: [presetWxapp()],
+
+export default defineConfig({
+  presets: [
+    presetAttributify(), // 开启属性模式(暂不支持小程序)
+    // 由 Iconify 提供支持的纯 CSS 图标解决方案
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetWxapp(), // 微信小程序预设
+  ],
   /**
    * 自定义快捷语句
    * @see https://github.com/unocss/unocss#shortcuts
@@ -10,4 +26,8 @@ export default {
       center: 'flex justify-center items-center',
     },
   ],
-}
+  transformers: [
+    transformerDirectives(), // 启用 @apply 功能
+    transformerVariantGroup(), // 启用 () 分组功能
+  ],
+})
