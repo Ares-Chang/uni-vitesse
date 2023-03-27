cae7360d6d59c930021879ea9ba0b97d2ee5008c
chang <2622310479@qq.com>
2023-03-27 14:18:57 +0800

    feat: created Hello World components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello World!')
+</script>
+
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">
+        {{ title }}
+      </text>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: pink;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,43 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello World!')
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">
-        {{ title }}
-      </text>
-    </view>
-  </view>
+  <div>
+    <HelloWorld />
+  </div>
 </template>
-
-<style lang="scss" scoped>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: pink;
-}
-</style>
