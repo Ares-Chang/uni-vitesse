1c816d64c40f88098194af45b97f52318d000efc
chang <2622310479@qq.com>
2023-03-27 14:07:33 +0800

    chore: config @antfu/eslint-config package

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
 onLaunch(() => {
-  console.log("App Launch");
-});
+  console.log('App Launch')
+})
 onShow(() => {
-  console.log("App Show");
-});
+  console.log('App Show')
+})
 onHide(() => {
-  console.log("App Hide");
-});
+  console.log('App Hide')
+})
 </script>
+
 <style></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,16 +1,18 @@
-<template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
-</template>
-
 <script setup lang="ts">
 import { ref } from 'vue'
 const title = ref('Hello World!')
 </script>
+
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">
+        {{ title }}
+      </text>
+    </view>
+  </view>
+</template>
 
 <style lang="scss" scoped>
 .content {
