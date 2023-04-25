e646742f62f0b134fb8b88760ee62f1ff83416b1
chang <2622310479@qq.com>
2023-04-25 13:33:49 +0800

    chore: remove console

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-onLaunch(() => {
-  console.log('App Launch')
-})
-onShow(() => {
-  console.log('App Show')
-})
-onHide(() => {
-  console.log('App Hide')
-})
+// At least one <template> or <script> is required in a single file component.
 </script>
 
 <style>
