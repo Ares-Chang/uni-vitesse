bc5defd9ba0f496a6edd4fe0ecd1b35ab3549fa5
chang <2622310479@qq.com>
2023-04-26 16:06:57 +0800

    chore: Fit dark mode

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
 // At least one <template> or <script> is required in a single file component.
 </script>
 
-<style>
+<style lang="scss">
 page {
   @apply: font-sans text-center box-border px-1rem py-2rem color-gray-700;
+}
+
+@media (prefers-color-scheme: dark) {
+  page {
+    @apply: color-gray-200;
+  }
 }
 </style>
