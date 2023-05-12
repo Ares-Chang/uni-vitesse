bf2e1cbcc73780b5e0d5694fd8d40795ca1e2551
chang <2622310479@qq.com>
2023-05-12 14:17:04 +0800

    chore: update deps

--- pages.config.ts
+++ pages.config.ts
@@ -1,13 +1,7 @@
 import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
 
 export default defineUniPages({
-  pages: [
-    // 修改默认排序置顶
-    {
-      path: 'pages/index',
-      type: 'home', // 设置 home 置顶
-    },
-  ],
+  pages: [],
   globalStyle: {
     'navigationBarTitleText': 'uni-vitesse',
     'navigationBarBackgroundColor': '@navBgColor',
