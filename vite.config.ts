09056e74c108c47b49b7a18514efe7767748f084
chang <2622310479@qq.com>
2023-05-23 12:02:48 +0800

    chore: config resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
@@ -11,6 +12,11 @@ import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [
     UniPages(),
     UniLayouts(),
