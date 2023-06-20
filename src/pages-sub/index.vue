4f0de9d9c35d22c0df0dec2b437e633894944978
chang <2622310479@qq.com>
2023-06-20 11:49:35 +0800

    chore: update desp && fix sub pages none layouts

--- src/pages-sub/index.vue
+++ src/pages-sub/index.vue
@@ -1,24 +1,10 @@
 <template>
-  <div
-    class="app" :class="{
-      dark: isDark,
-    }"
-  >
-    <main
-      px-4 py-10
-      font-sans text="center"
-      color-gray-700 dark:color-gray-200
-    >
-      <div mb-6>
-        <h1 mb-2>
-          Sub Page
-        </h1>
-        <p text-sm op75 italic>
-          <em>Sub Page Content</em>
-        </p>
-      </div>
-
-      <TheFooter />
-    </main>
+  <div>
+    <h1 mb-2>
+      Sub Page
+    </h1>
+    <p text-sm op75 italic>
+      <em>Sub Page Content</em>
+    </p>
   </div>
 </template>
