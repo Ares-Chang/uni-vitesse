0d8592e8c1ba0e87172d5dfe3b5b77b7994c529b
chang <2622310479@qq.com>
2023-08-02 15:32:31 +0800

    perf: useRouter created mode change

--- src/composables/useRouter.ts
+++ src/composables/useRouter.ts
@@ -1,6 +1,6 @@
 /**
  * 路由跳转
- * @param {string} path 路由地址 pages 目录下可省略 pases，子包目录下需要以 ~ 开头，如：~/pages-sub/index
+ * @param {string} path 目标路由地址
  * @param {Record<string, any>} query 路由参数
  * @param {boolean} replace 是否替换当前页面
  * @param {boolean} tabBar 是否跳转到 tabBar 页面
@@ -12,20 +12,14 @@ interface Router {
   tabBar?: boolean
 }
 
-class UseRouter {
-  webview: string
-
-  constructor(config: { webview: string }) {
-    this.webview = config.webview
-  }
-
-  private setQuery(params: Record<string, any>) {
+function useRouter(config: { webview: string }) {
+  function setQuery(params: Record<string, any>) {
     return Object.entries(params) // 将对象转换成 [key, value] 数组
       .map(([key, value]) => `${encodeURI(key)}=${encodeURI(JSON.stringify(value))}`) // 将每个数组元素转换成 key=value 字符串，需要对 value 进行 JSON 序列化和 URL 编码
       .join('&') // 将数组用 & 符号连接成字符串
   }
 
-  push(arg: string | Router) {
+  function push(arg: string | Router) {
     let url = ''
     let replace = false
     let tabBar = false
@@ -33,7 +27,7 @@ class UseRouter {
       url = arg
     }
     else {
-      const queryParams = this.setQuery(arg?.query || {})
+      const queryParams = setQuery(arg?.query || {})
       url = `${arg?.path}?${queryParams}`
       replace = arg?.replace || false
       tabBar = arg?.tabBar || false
@@ -46,9 +40,9 @@ class UseRouter {
       // #endif
 
       // #ifndef H5
-      if (this.webview) {
+      if (config.webview) {
         uni.navigateTo({
-          url: `${this.webview}?url=${url}`,
+          url: `${config.webview}?url=${url}`,
         })
       }
       else { throw new Error('请先配置 webview 路由地址') }
@@ -66,22 +60,28 @@ class UseRouter {
       uni.navigateTo({ url })
   }
 
-  replace(params: string | Router) {
+  function replace(params: string | Router) {
     const arg = typeof params === 'string' ? { path: params } : params
 
-    this.push({
+    push({
       ...arg,
       replace: true,
     })
   }
 
-  back(delta = 1) {
+  function back(delta = 1) {
     uni.navigateBack({
       delta,
     })
   }
+
+  return {
+    push,
+    replace,
+    back,
+  }
 }
 
-export const router = new UseRouter({
+export const router = useRouter({
   webview: '/pages/webview',
 })
