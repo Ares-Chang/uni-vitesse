790de996fbb65cdebe9f95f6630c1998dccff73b
chang <areschang114@qq.com>
2023-11-13 13:24:51 +0800

    chore(eslint): Adjust component name convention

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,12 @@ const uni = require('@uni-helper/eslint-config')
 const unocss = require('@unocss/eslint-plugin')
 
 module.exports = uni(
-  {},
+  {
+    overrides: {
+      uni: {
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      },
+    },
+  },
   unocss.configs.flat,
 )
