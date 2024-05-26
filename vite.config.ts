4ef7f78126bd15cef4dc73bef09b3a1a669357eb
chang <areschang114@qq.com>
2024-05-26 15:45:14 +0800

    chore: update UnoCSS version & fix uni compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import UniPages from '@uni-helper/vite-plugin-uni-pages'
@@ -11,75 +10,83 @@ import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${resolve(__dirname, 'src')}/`,
-    },
-  },
-  plugins: [
-    /**
-     * vite-plugin-uni-pages
-     * @see https://github.com/uni-helper/vite-plugin-uni-pages
-     */
-    UniPages({
-      subPackages: [
-        'src/pages-sub',
-      ],
-    }),
-
-    /**
-     * vite-plugin-uni-layouts
-     * @see https://github.com/uni-helper/vite-plugin-uni-layouts
-     */
-    UniLayouts(),
-
-    /**
-     * unocss
-     * @see https://github.com/antfu/unocss
-     * see unocss.config.ts for config
-     */
-    Unocss(),
-
-    /**
-     * unplugin-auto-import 按需 import
-     * @see https://github.com/antfu/unplugin-auto-import
-     */
-    AutoImport({
-      imports: [
-        'vue',
-        'uni-app',
-      ],
-      dts: true,
-      dirs: [
-        './src/composables',
-      ],
-      vueTemplate: true,
-    }),
-
-    /**
-     * unplugin-vue-components 按需引入组件
-     * 注意：需注册至 uni 之前，否则不会生效
-     * @see https://github.com/antfu/vite-plugin-components
-     */
-    Components({
-      dts: 'src/components.d.ts',
-    }),
-
-    /**
-     * vite-plugin-vue-devtools 增强 Vue 开发者体验
-     * @see https://github.com/webfansplz/vite-plugin-vue-devtools
-     */
-    VueDevTools(),
-
-    uni(),
-  ],
-
+export default defineConfig(async () => {
   /**
-   * Vitest
-   * @see https://github.com/vitest-dev/vitest
+   * 为兼容 @dcloudio/vite-plugin-uni 采用 CJS ，而 UnoCSS 只支持 ESM
+   * @see https://github.com/dcloudio/uni-app/issues/4815
    */
-  test: {
-    environment: 'jsdom',
-  },
+  const Unocss = await import('unocss/vite').then(i => i.default)
+
+  return {
+    resolve: {
+      alias: {
+        '~/': `${resolve(__dirname, 'src')}/`,
+      },
+    },
+    plugins: [
+      /**
+       * vite-plugin-uni-pages
+       * @see https://github.com/uni-helper/vite-plugin-uni-pages
+       */
+      UniPages({
+        subPackages: [
+          'src/pages-sub',
+        ],
+      }),
+
+      /**
+       * vite-plugin-uni-layouts
+       * @see https://github.com/uni-helper/vite-plugin-uni-layouts
+       */
+      UniLayouts(),
+
+      /**
+       * unocss
+       * @see https://github.com/antfu/unocss
+       * see unocss.config.ts for config
+       */
+      Unocss(),
+
+      /**
+       * unplugin-auto-import 按需 import
+       * @see https://github.com/antfu/unplugin-auto-import
+       */
+      AutoImport({
+        imports: [
+          'vue',
+          'uni-app',
+        ],
+        dts: true,
+        dirs: [
+          './src/composables',
+        ],
+        vueTemplate: true,
+      }),
+
+      /**
+       * unplugin-vue-components 按需引入组件
+       * 注意：需注册至 uni 之前，否则不会生效
+       * @see https://github.com/antfu/vite-plugin-components
+       */
+      Components({
+        dts: 'src/components.d.ts',
+      }),
+
+      /**
+       * vite-plugin-vue-devtools 增强 Vue 开发者体验
+       * @see https://github.com/webfansplz/vite-plugin-vue-devtools
+       */
+      VueDevTools(),
+
+      uni(),
+    ],
+
+    /**
+     * Vitest
+     * @see https://github.com/vitest-dev/vitest
+     */
+    test: {
+      environment: 'jsdom',
+    },
+  }
 })
